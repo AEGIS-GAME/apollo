28ea0bee64a80823b96b6ca225923d03815d079b
Dante <85536418+daannte@users.noreply.github.com>
2025-08-19 13:06:52 -0600

    Nestjs (#18)
    * refactor: switch to nestjs
    
    * test: add user service tests

--- athena/backend/migrations/000001_init.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT exists users (
-    id TEXT PRIMARY KEY, -- UUID stored as string
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    is_admin BOOLEAN NOT NULL DEFAULT FALSE
-);
---
+++ athena/backend/src/users/users.service.ts
@@ -0,0 +1,48 @@
+import { ConflictException, Inject, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { Kysely } from "kysely";
+import { DB } from "src/db/types";
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+
+@Injectable()
+export class UsersService {
+  constructor(@Inject("DB") private readonly db: Kysely<DB>) { }
+
+  private readonly SALT_ROUNDS = 10
+
+  async create(username: string, password: string) {
+    const existing = await this.db
+      .selectFrom("users")
+      .select("id")
+      .where("username", "=", username)
+      .executeTakeFirst()
+
+    if (existing) throw new ConflictException("Username already taken")
+
+    const password_hash = await bcrypt.hash(password, this.SALT_ROUNDS)
+
+    const user = await this.db
+      .insertInto("users")
+      .values({ username, password_hash, is_admin: false })
+      .returningAll()
+      .executeTakeFirst()
+
+    if (!user) {
+      throw new InternalServerErrorException('Failed to create user');
+    }
+
+    return user
+  }
+
+  generateTokens(userId: number) {
+    const accessToken = jwt.sign({ sub: userId }, process.env.ACCESS_TOKEN_SECRET!, {
+      expiresIn: "15m"
+    })
+
+    const refreshToken = jwt.sign({ sub: userId }, process.env.REFRESH_TOKEN_SECRET!, {
+      expiresIn: "7d"
+    })
+
+    return { accessToken, refreshToken }
+  }
+}
