a14cdf1255d8a52962ec5d0d76b9acff96432a2f
Dante <coho.funny_6i@icloud.com>
2025-08-19 12:32:45 -0600

    refactor: switch to nestjs

---
+++ athena/backend/src/db/db.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { Kysely, SqliteDialect } from "kysely";
+import { DB } from "./types";
+import Database from "better-sqlite3";
+
+@Module({
+  providers: [
+    {
+      provide: "DB",
+      useValue: new Kysely<DB>({
+        dialect: new SqliteDialect({ database: new Database("dev.db") })
+      })
+    }
+  ],
+  exports: ["DB"]
+})
+export class DbModule { }
